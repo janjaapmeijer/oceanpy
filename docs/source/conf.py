178673552db2381544785bb0066d0ba10cae957d
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2021-09-29 18:32:25 +1000

    Add numpy docstring.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon']
 
+napoleon_numpy_docstring = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
