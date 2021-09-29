385f15854ed0db524b8b2dd0197a91afd0fc6616
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2021-09-29 19:24:01 +1000

    Mock oceanpy.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-# autodoc_mock_imports = ['oceanpy']
+autodoc_mock_imports = ['oceanpy']
 
 # -- Project information -----------------------------------------------------
 
