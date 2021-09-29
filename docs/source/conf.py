5e02b9124a400bd7600978fa33e04704e289a704
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2021-09-29 20:42:49 +1000

    Add additional path.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../..'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../models'))
 # autodoc_mock_imports = ['oceanpy']
 
 # -- Project information -----------------------------------------------------
