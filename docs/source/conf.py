9c364b66c470f796c6adadfec60a9fc565604156
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2021-09-29 18:00:43 +1000

    Add oceanpy to autodoc_mock_imports.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../oceanpy'))
+autodoc_mock_imports = ['oceanpy']
 
 # -- Project information -----------------------------------------------------
 
