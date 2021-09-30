ab379913b08d582d55eecb0ba549ab2a5f0ae043
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2021-09-30 14:39:11 +1000

    Changed init.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,21 @@
+#__all__ = ['folder1', 'folder2'] # for folders within OceanPy
+
+# from . import colormaps
+# from . import extreme_value_analysis
+# from . import find
+# from . import interpolation
+# from . import maps
+# from . import meteorology
+# from . import animations
+# from . import plot
+# from . import projections
+# from . import readwrite
+# from . import shapefiles
+# from . import remote
+# from . import grid
+# from . import polynomials
+# from .netcdf import *
+from .tools import *
+
 __author__ = 'Jan Jaap Meijer'
 __email__ = 'janjaapmeijer@gmail.com'
