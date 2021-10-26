32efdad26efa898e6afb30fc0cbc9550930cef07
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2021-10-26 18:13:46 +1100

    Cross-section velocity coordinate changes.

--- tools/contour.py
+++ tools/contour.py
@@ -13,9 +13,8 @@ class Contour(object):
             self.dataset = xr.open_dataset(filename_obj)
         except ValueError:
             self.dataset = filename_obj
-
         if coords is None:
-            coords = list(self.dataset.coords.keys())
+            self.coords = list(self.dataset.coords.keys())
             # if len(coords) == 2:
             #     self.coords_contour = coords
             # else:
@@ -24,8 +23,9 @@ class Contour(object):
             #         %coords)
         else:
             self.coords = coords
+        # self.length = self.contour_length(contour_name, contour_value)
 
-        # self.get_coords(contour_name=contour_name, contour_value=contour_value)
+        # self.cont_coords = self.get_coords(contour_name, contour_value)
         #     pass
 
     def get_coords(self, contour_name, contour_value,
@@ -103,6 +103,17 @@ class Contour(object):
         if coords is None:
             coords = self.coords
 
+        # to make a coordinate 2 dimensions are needed,
+        # adjusts shape of coordinate in case the size =1
+        coord_sizes = np.array([da[coord].size for coord in coords])
+        if any(coord_sizes == 1) and coord_sizes.size == 2:
+            shp, idx = np.max(coord_sizes), np.argmin(coord_sizes)
+            da = da.assign({coords[idx]: np.broadcast_to(da[coords[idx]], shp)})
+            if not coord_sizes.size == 2:
+                raise ValueError(
+                    'To make a 1 dimension sectional coordinate 2 coordinates, should be given, but found %s'
+                    %coord_sizes.size)
+
         # make contour coordinate along selected contour points
         # try:
         distance = haversine(da[coords[0]], da[coords[1]])[0]
@@ -114,18 +125,18 @@ class Contour(object):
 
         return da.assign_coords({coord_name: cont_coord})
 
-    def length(self, contour_value):
+    def contour_length(self, contour_name, contour_value):
 
         """
-        Calculate length of specific contour with contour_value.
+        Calculate length of specific contour.
         """
 
         try:
             self.cont_coords
         except:
-            self.get_coords()
+            self.get_coords(contour_name, contour_value)
 
-        self.length = np.sum(haversine(cont_coords[:, 0], cont_coords[:,1])[0])
+        return np.sum(haversine(self.cont_coords[:, 0], self.cont_coords[:,1])[0])
 
     def cross_section(self, coord_sel, npnts, spacing,
                       section_name='cross-section', var_name=None, coords=None, transform=None, decompose=False):
@@ -150,9 +161,8 @@ class Contour(object):
         idx = np.argmin([haversine([ln, coord_sel[0]], [lt, coord_sel[1]])[0]
                          for ln, lt in self.cont_coords])
         bearing = haversine(*zip(*self.cont_coords))[1]
-        # orientation = self.bearing2standard(bearing[idx])
         orientation = bearing_to_standard(bearing[idx-1:idx])/2
-        orientation = orientation - (np.pi/2)
+        orientation = orientation - (np.pi/2) if (np.pi/2) < orientation <= (3*np.pi/2) else orientation + (np.pi/2)
 
         # transform center point of transect to cartesian coordinate
         WGS84 = pyproj.Proj('EPSG:4326')
@@ -173,6 +183,7 @@ class Contour(object):
         lat_st = np.array(lat_st)
         if all(self.dataset[coords[0]] < 0):
             lon_st = np.array(lon_st) - 360
+
         cross_cont_pnt = []
         for x, y in zip(lon_st, lat_st):
             cross_cont_pnt.append(
