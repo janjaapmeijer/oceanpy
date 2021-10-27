1c2228aa8245f088937a126401d72a4b4370b7fd
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2021-10-27 17:42:26 +1100

    Make cartesian to polar and natural coordinates available to all.

--- obs/satellite.py
+++ obs/satellite.py
@@ -13,7 +13,7 @@ from gsw import f, grav
 
 # from . import createNetCDF
 # from . import haversine, rotatexy
-
+from . import cartesian_to_polar, polar_to_cartesian
 __all__ = ['gradient_wind_from_ssh',
            'qg_from_ssh',
            ]
